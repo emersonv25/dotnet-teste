1a424c201491c7100f95cdb3cda5870650ddf51b
emersonv25 <emersondejesussantos@hotmail.com>
2023-07-11 11:38:51 -0300

    teste

--- api-teste/Controllers/WeatherForecastController.cs
+++ api-teste/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace api_teste.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "um", "dois", "tres", "4", "5", "6", "7", "8", "9", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
