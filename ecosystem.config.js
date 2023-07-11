7c4254a6beb80e23477600ad79824f7ec094ebd5
emersonv25 <emersondejesussantos@hotmail.com>
2023-07-11 11:36:29 -0300

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,6 @@ module.exports = {
         {
             name : "api-teste",
             script : "runApi.sh",
-            cwd: "./api-teste/bin/Release/net6.0/",
             max_memory_restart : "1G"
         }
     ]
